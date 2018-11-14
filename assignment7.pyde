4e2f445b68b1131a70af9081958cfd3228586fa6
Francesco Aiello <aiellof@oregonstate.edu>
2018-11-14 14:37:25 -0800

    graphical binary search tree with flashing lights

--- assignment7.pyde
+++ assignment7.pyde
@@ -1,6 +1,21 @@
 from tree import CoolTree
 
-t = CoolTree(500, 500)
+bst = CoolTree(400, 50, 25)
+bst.insert(CoolTree(350, 80, 12))
+bst.insert(CoolTree(300, 110, 8))
+bst.insert(CoolTree(380, 110, 13))
+bst.insert(CoolTree(450, 80, 60))
+bst.insert(CoolTree(420, 110, 58))
+bst.insert(CoolTree(500, 110, 72))
+bst.insert(CoolTree(250, 140, 1))
+bst.insert(CoolTree(330, 140, 9))
+bst.insert(CoolTree(360, 140, 12.5))
+bst.insert(CoolTree(390, 140, 14))
+bst.insert(CoolTree(410, 140, 55))
+bst.insert(CoolTree(435, 140, 59))
+bst.insert(CoolTree(470, 140, 70))
+bst.insert(CoolTree(550, 140, 81))
+
 
 def setup():
     size(800, 800)
@@ -8,5 +23,5 @@ def setup():
 def draw():
     global t
     background(0)
-    t.update()
-    t.draw()
+    bst.update()
+    bst.draw()
--- tree.py
+++ tree.py
@@ -1,12 +1,40 @@
+import random
+
 class CoolTree:
     
-    def __init__(self, x = 100, y = 100):
+    def __init__(self, x, y, value = None):
         self.x = x
         self.y = y
+        self.value = value
+        self.left = None
+        self.right = None
+    def insert(self, child):
+        if self.value > child.value:
+            if self.left == None:
+                self.left = child
+            else:
+                self.left.insert(child)
+        else:
+            if self.right == None:
+                self.right = child
+            else:
+                self.right.insert(child)
         
     def update(self):
-        self.x += 2
-        
+        pass      
+                        
     def draw(self):
-        ellipse(self.x, self.y, 100, 100)
+        if self.left is not None:
+            stroke(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            fill(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            line(self.x, self.y, self.left.x, self.left.y)
+            ellipse(self.x, self.y, 20, 20)
+            self.left.draw()
+        if self.right is not None:
+            stroke(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            fill(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            line(self.x, self.y, self.right.x, self.right.y)
+            ellipse(self.x, self.y, 20, 20)
+            self.right.draw()        
+        
         
